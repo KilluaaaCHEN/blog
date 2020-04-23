cb8d7cfc1330ce9ba552bbd442c18fa99a6c408a
KilluaChen <killua_cz@163.com>
2020-04-23 20:09:09 +0800

    backend

--- routes/route.go
+++ routes/route.go
@@ -3,9 +3,13 @@ package routes
 import (
 	"gin/ctrl"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func LoadRoute(r *gin.Engine) {
+
+	r.LoadHTMLGlob("backend/public/index.html")
+	r.StaticFS("/admin", http.Dir("./backend/public"))
 
 	user := r.Group("user")
 	{
