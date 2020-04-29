7feef3d9cec9110a64095c60205dc16f976d93dc
KilluaChen <killua_cz@163.com>
2020-04-29 13:52:19 +0800

    posts

--- api/ctrl/Post.go
+++ api/ctrl/Post.go
@@ -6,7 +6,6 @@ import (
 	"blog/common/validates"
 	"blog/dao"
 	"github.com/gin-gonic/gin"
-	"net/http"
 	"strings"
 )
 
@@ -16,11 +15,6 @@ type Post struct {
 func (u *Post) List(c *gin.Context) {
 
 	contextInfo, _ := c.Get("context")
-	switch contextInfo.(type) {
-	case error:
-		c.JSON(http.StatusBadRequest, gin.H{"err": "param_error", "err_msg": contextInfo.(error).Error()})
-		return
-	}
 	context := contextInfo.(validates.ListParam)
 
 	pageSize, totalCount := 5, 0
